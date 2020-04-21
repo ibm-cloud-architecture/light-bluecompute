87dffaa65e18ecc05855c6baafae6217bf6c165e
Hemankita Perabathini <Hemankita1@Hemankitas-MacBook-Pro-2.local>
2020-04-21 15:51:50 +0530

    adding docker changes

--- catalog/startup.sh
+++ catalog/startup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-set -e
+source scripts/max_heap.sh
+
+# Set Max Heap
+export JAVA_OPTS="${JAVA_OPTS} -Xmx${max_heap}m"
 
 # Set basic java options
-export JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom"
+export JAVA_OPTS="${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom"
 
-echo "Starting with Java Options ${JAVA_OPTS}"
+# Set java.io.tmpdir to other than /tmp
+if [ -n "$JAVA_TMP_DIR" ]; then
+	export JAVA_OPTS="${JAVA_OPTS} -Djava.io.tmpdir=$JAVA_TMP_DIR"
+fi
+
+echo "Starting Java Application"
 
 # Start the application
-exec java ${JAVA_OPTS} -jar /app.jar
-
+exec java ${JAVA_OPTS} -jar ./app.jar
